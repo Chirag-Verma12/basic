eeacf8028eaceac3ab73b9181b49ea31de5d19b0
Chirag-Verma12 <cherryverna49544@gmail.com>
2025-08-24 16:06:35 +0530

    correct

--- Django/projects/views.py
+++ Django/projects/views.py
@@ -6,13 +6,13 @@ def project_index(request):
     context = {
         "projects": projects
     }
-    return render(request, "projects/project_index.html", context)
+    return render(request, "projects/projects_index.html", context)
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     context = {
         "project": project
     }
-    return render(request, "projects/project_detail.html", context)
+    return render(request, "projects/projects_detail.html", context)
 
 # Create your views here.
