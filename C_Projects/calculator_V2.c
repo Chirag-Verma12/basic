1ef216ad883b67bdee2d4ff013dd4d4df4e9d9dc
Chirag-Verma12 <cherryverna49544@gmail.com>
2026-01-12 03:29:17 +0530

    Calculator using c

---
+++ C_Projects/calculator_V2.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+//main code that will be uploaded to github
+int main(){
+    float a, b;
+    int choice;
+    char again;
+    float result;
+    char invalid;
+    char invalid_n2;
+
+    printf("\n");
+    //intro 
+    printf("***<>***<>***<>***<>***<>***<>***<>");
+    printf("\n    Welcome to Calculator\n");
+    printf("***<>***<>***<>***<>***<>***<>***<>");
+    printf("\n");
+    printf("\n");
+    printf("     Instruction :");
+    printf("\n=======================");
+    printf("\nIf you enter an alpabet the code will crash");
+    printf("\n");
+
+    do{
+        do{
+            printf("\nNumber 1: ");
+            scanf("%f", &a);
+            if(a >= 1){
+                break;
+            } else{
+                printf("\nInvalid Input");
+                printf("\nWanna try again? (y /n) ->\t");
+                scanf(" %c", &invalid);
+            }
+        }while(invalid == 'y' || invalid == 'Y');
+        if(invalid =='n' || invalid == 'N'){
+            break;
+        }
+        
+        
+        do{
+            printf("Number 2 = ");
+            scanf("%f", &b);
+            if(b >= 1){
+                break;
+            } else{
+                printf("\nInvalid Input");
+                printf("\nWanna try again? (y /n) ->\t");
+                scanf(" %c", &invalid_n2);
+            }
+        }while(invalid_n2 == 'y' || invalid_n2 == 'Y');
+        if(invalid_n2 == 'n' || invalid_n2 == 'N'){
+            break;
+        }
+    
+        printf("\n--------");
+        printf("\n  MENU:\n");
+        printf("--------\n");
+        printf("1. Addition\n2. Subtraction\n3. Multiply\n4. Divide\n");
+
+        //taking the input from the user to perfrom operation
+    
+        printf("Choose ->\t");
+        if(scanf("%d", &choice) != 1){
+            printf("\nInvalid Input\n");
+            break;
+        }
+
+        switch(choice){
+            case 1: 
+            result = a + b;
+            printf("\nThe Answer is =%.2f", result);
+            break;
+            case 2: 
+            result = a-b;
+            printf("\nThe Answer is=%.2f", result);
+            break;
+            case 3: 
+            result = a*b;
+            printf("\nThe Answer is=%.2f", result);
+            break;
+            case 4: 
+            if(b == 0){
+                printf("\nInvalid Input\n");
+                break;
+            }
+            result = a/b;
+            printf("The Answer is=%.2f", result);
+            break;
+            default: printf("\nInvalid Number\n");
+        }
+        printf("\n");
+        printf("\nDo u want to do again? (y/ n) ->\t");
+        scanf(" %c", &again);
+        if(again == 'n' || again == 'N'){
+            printf("\n-+-+-+-+-+-+-+-+-+-+-+-+");
+            printf("\n       Thank you");
+            printf("\n-+-+-+-+-+-+-+-+-+-+-+-+");
+        }
+    } while(again == 'y' || again == 'Y');
+    return 0;
+}
