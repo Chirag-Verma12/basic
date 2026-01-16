94cf1ff96ba522a297611a9cb293a581073ecca5
Chirag-Verma12 <cherryverna49544@gmail.com>
2026-01-16 15:34:01 +0530

    Fixing Loop

--- python_project/Number_Guessing.py
+++ python_project/Number_Guessing.py
@@ -1,20 +1,21 @@
 import random
 print("*************************************")
+print("-------------------------------------")
 print("   Welcome To Number Guessing Game")
+print("-------------------------------------")
 print("*************************************")
 
 #generating the number
 lives = 7
 attempt = 0
-again = []  
+again = " "  
 random_number = int(random.randint(1, 100))
-print(random_number) #hide after done
-print(f"\nYour TOTAL LIFE: {lives}\n")
+print(f"\nYour TOTAL LIFE: {lives}")
 
 while True:
-    while True:
+    while True or again == "yes":
         try:
-            guess = int(input("Guess:\t"))
+            guess = int(input("Guess:"))
             if(guess > 1):
                 break
             else:
@@ -22,28 +23,42 @@ while True:
 
         except ValueError:
             print("Invalid input, please keep a number")
-    if(lives <= 1):
-        print("LOSS")
-        print(f"the actual number was {random_number}")
-        break
 
-    elif(guess == random_number):
-        print("YOU WON")
+    if(guess == random_number):
+        print("\nYOU WON")
         attempt +=1
-        print(f"YOU WON AT {lives} remaining")
+        print(f"🎉 YOU WON in {lives} Remaining")
         print(f"Attempts you have taken {attempt}")
+
 
     elif(guess > random_number):
         lives -=1
         attempt +=1
         print("HIGHER Than the Actual Number")
-        print(f"LIFE LEFT: {lives}")
+        print(f"\nLIFE LEFT: {lives}")
 
     elif(guess < random_number):
         lives -=1
         attempt +=1
         print("LOWER Than The Actual Number")
-        print(f"LIFE LEFT: {lives}")
+        print(f"\nLIFE LEFT: {lives}")
+    
+    if(lives == 0):
+            print("\nYou LOSS >_<")
+            print(f"The Actual Number Was {random_number}")
+    
+    while lives < 1 or guess == random_number:
+        random_number = int(random.randint(1, 100))
+        lives = 7
+        attempt = 0
+        again = input("\nDo You wanna play again?? (y/ n):")
+
+    if(again == "N" or again == "n" or again == "no" or again == "NO"):
+        print("<><><><><><><><><><><><><><><><><><>")
+        print("\n        Thank for playing\n")
+        print("<><><><><><><><><><><><><><><><><><>")
+        break
+        
     
 
         
