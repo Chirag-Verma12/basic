6edb8183630117c159a055863c1acbcbeb0b960c
Chirag-Verma12 <cherryverna49544@gmail.com>
2026-02-08 02:14:50 +0530

    Some changes, Still in Progress

--- python_project/Advance_NumberGuessing.py
+++ python_project/Advance_NumberGuessing.py
@@ -1,5 +1,6 @@
 import random
 from colorama import Fore, init, Style
+from sympy import Le
 init(autoreset=True)
 print(Fore.GREEN + "*************************************")
 print("-------------------------------------")
@@ -7,35 +8,40 @@ print(Fore.BLUE + "   Welcome To Number Guessing Game")
 print("-------------------------------------")
 print(Fore.GREEN + "*************************************")
 
-#generating the number
+#defining variables 
 easy_Lives = 7
 medium_Lives = 5
 hard_Lives = 4
 score = 0
 attempt = 0
 again = " "  
+guess = 2
 win = 0
 Level = 0
-random_number = int(random.randint(1, 100))
-print(Fore.RED + f"\nYour TOTAL LIFE: {easy_Lives}")
+random_number = int(random.randint(1, 100)) #generating the number
 
 #Main Logic
 while True:
     #choosing the Diffculty level
-    print("Input The Diffculty You Want..\n1. Easy\n2. Medium\n3. Hard\n")
+    print("Input The Diffculty You Want..\n1. Easy\n2. Medium\n3. Hard\n4. Exit\n")
     while True:
         try:
             Level = int(input("Enter The Number Of Diffculty ->"))
-            if(Level >= 4):
+            if(Level >= 5):
                 print(Fore.RED + "\nInvalid Input!!!")
             else:
                 break
         except ValueError:
             print(Fore.RED + "\nInvalid input, please keep a number")
 
+    if(Level == 4):
+        print("thank you fro playing")
+        break
+
     while(Level == 1):
-        print(random_number)
+        # print(random_number) Answer revealing
         #Tells invalid input by the user, valid input must be > 1
+        print(Fore.RED + f"\nLives: {easy_Lives}" + Fore.RESET)
         while True:
             try:
                 guess = int(input(Fore.YELLOW + "Guess:" + Fore.RESET))
@@ -49,11 +55,14 @@ while True:
             print(Fore.CYAN + "\nYOU WON")
             attempt +=1
             win +=1 #counts the win 
+            score +=15
             print(Fore.GREEN + f"🎉 YOU WON in {easy_Lives} Remaining,")
-            print(f"Attempts you have taken {attempt}")
-            print(f"UR SCORE: {score}")
+            print(f"SCORE: {score}")
+            print(f"Attempts you have taken {attempt}\n")
             if(win > 1): #win more than one, it count it as a streak and print the streak
+                score +=9
                 print(f"\nYour Streak is going crazy!!! {win}, Keep Going Champ\n")
+            break
 
         elif(guess > random_number):
             attempt +=1
@@ -64,28 +73,36 @@ while True:
             else:
                 print(Fore.MAGENTA + "HIGHER Than the Actual Number")
                 score +=6 #if, user is closer... it will add +6
-            print(f"\nLIFE LEFT: {easy_Lives}")
 
         elif(guess < random_number):
             easy_Lives -=1
             attempt +=1
-            if(random_number > 50 and guess < 30):
+            if(random_number > 20 and guess > 60):
                 print("Huge diiiiiiffff")
                 score +=4
             else:
                 print(Fore.BLUE + "LOWER Than The Actual Number")
                 score +=6
-            print(f"\nLIFE LEFT: {easy_Lives}")
 
         if(easy_Lives == 0):
             print("\nYou LOSS >_<")
             print(f"The Actual Number Was {random_number}")
+            print(f"Your score {score}")
             if(win > 1):
                 print(f"Your wining streak {win}, DO IT AGAIN...")
             break
 
+    if(easy_Lives == 0 or guess == random_number):
+        again = input("DO you wanna play again yes or no answer respectively? (y/ n)" )
+        if(again == "y"):
+            random_number = int(random.randint(1, 100))
+        if(again == "n"):
+            break
+
+    random_number = int(random.randint(1, 110)) 
+    print(Fore.RED + f"\nYour TOTAL LIFE: {hard_Lives}")
     while Level == 3:
-        print(random_number)
+        # print(random_number) Answer's
         while True:
             try:
                 guess = int(input(Fore.YELLOW + "Guess:" + Fore.RESET))
