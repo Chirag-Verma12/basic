eb83799b744c49f67c1b6f9fad8e301cdc1c1ddf
Chirag-Verma12 <cherryverna49544@gmail.com>
2026-03-15 17:55:18 +0530

    Password Strength Checker

--- python_project/PasswordStrengthChecker.py
+++ python_project/PasswordStrengthChecker.py
@@ -6,11 +6,10 @@ init(autoreset=True)
 password = " "
 password_good = False
 
-#scoring the password
-score = 0
-
 #main logic
 while password_good == False:
+    #scoring the password
+    score = 0
 
     #defined requirements of a good password, and when the loops starts again all the things reset
     requirements= {
@@ -18,6 +17,7 @@ while password_good == False:
         "CountS_C" : 0,
         "uppercase" : 0,
         "spaces" : 0,
+        "lowercase" : 0
     }
 
     #user enter's password
@@ -32,7 +32,6 @@ while password_good == False:
         if requirements["Special_Character"] in password[i]:
             requirements["CountS_C"] +=1
             score += 2
-        
     if requirements["CountS_C"] < 1:
         print(Fore.RED + "At least one specail character must be in password")
 
@@ -48,21 +47,35 @@ while password_good == False:
         if password[u].isupper():
             requirements["uppercase"] +=1
     if requirements["uppercase"] < 1:
-        print("There must be a uppercase in password")
+        print(Fore.RED + "Cant be full lowercase")
+
+    #checks for lowercase in password
+    for l in range(len(password)):
+        if password[l].islower():
+            requirements["lowercase"] +=1
+    if requirements["lowercase"] < 1:
+        print(Fore.RED + "Cant Be full uppercase")
 
     #checks for number or int value in password
     count_digit = 0
     for n in range(len(password)):
         if password[n].isdigit():
-            # print("Digit Found: ", password[n])
             count_digit +=1
             score += 2
-        # print("Number Count = ", count_digit)
     
-    if (requirements["CountS_C"] and requirements["uppercase"]) >=1 and count_digit >=1:
+    if count_digit == 0:
+        print(Fore.RED + "There must be digit in your password")
+    
+    if (requirements["CountS_C"] and requirements["uppercase"] and requirements["lowercase"]) >=1 and count_digit >=1 and requirements["spaces"] == 0:
         print("\nNow the password is ready for use\n")
         password_good = True
 
 #score the password 
 print(f"Password = {password}")
 print(f"Score = {score}")
+
+if score <10:
+    print(Fore.YELLOW + "Use of more special character & number can make your password more strong")
+
+if score >= 10:
+    print(Fore.GREEN +"Excellent Strong Password")
