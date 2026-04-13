043276148add6b67ce1da912be996de878fdd7b1
Chirag-Verma12 <cherryverna49544@gmail.com>
2026-04-13 23:15:39 +0530

    Fixed Functions and Loops

--- python_project/Contact_Book.py
+++ python_project/Contact_Book.py
@@ -1,85 +1,103 @@
 from colorama import Fore, init
+init(autoreset=True)
 import json
 import os
-init(autoreset=True)
 
-#Storing Data (OOPS)
-class info:
-    def __init__(self, name, phone_number, city) -> None:
-        self.name = name
-        self.phone_number = phone_number
-        self.city = city
+#import file name as FILE
+FILE = "data.json"
 
+#loading file
+def loading_file():
+    if not os.path.exists(FILE) or os.path.getsize(FILE) == 0:
+        return[]
+    with open(FILE, "r") as file:
+        return json.load(file)
+    
+#saving contact
+def save_contact(contacts):
+    with open(FILE, "w") as file:
+        json.dump(contacts, file, indent=4)
 
-#Welcome and Menu
-print("\n")
-print(Fore.BLACK + "=================")
-print(Fore.GREEN + "   Contact Book")
-print(Fore.BLACK + "=================")
-print("\b")
+#adding contact 
+def add_contact():
+    correct_contact_number = False
+    name = input("Enter name -> ")
+    while correct_contact_number == False:
+        phone_number = input("Phone number -> ")
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            print("Incorrect Contact Number\n")
+        else:
+            correct_contact_number = True
+            break
+        
+    city = input("Enter city -> ")
 
-#dictionary to store information in list 
-information = {
-        "name" : [],
-        "phone_number" : [],
-        "city" : []
+    contact = {
+        "name" : name,
+        "phone_number" : phone_number,
+        "city" : city
     }
 
-while True:
-    contact_info_saved = False
-    menu_select = 0
+    contacts = loading_file()
+    contacts.append(contact)
+    save_contact(contacts)
+    print(f"{name} saved sucessfully!!\n")
+
+#display contacts
+def display_contact():
+    contacts = loading_file()
+    if not contacts:
+        print("Nothing has been saved yet!\n")
+        return
+    for i, c in enumerate(contacts, 1):
+        print(f"\n{i}. {c['name']} | {c['phone_number']} | {c['city']}\n")
+
+#searching
+def search_contact():
+    search = input("Enter name to search -> ")
+    contacts = loading_file()
+
+    if not contacts:
+        print("Nothing has been saved yet!!\n")
+        return
     
+    result = [c for c in contacts if search in c["name"].lower()]
+    if result:
+        for c in result:
+            print(f"\nName: {c['name']}")
+            print(f"\nPhone number: {c['phone_number']}")
+            print(f"\nCity: {c['city']}")
+    else:
+        print("Contact not found!\n")
+
+#delete contact
+def delete_contact():
+    name = input("Enter name to delete contact -> ")
+    contacts = loading_file()
+    new_contact = [c for c in contacts if c["name"].lower() != name]
+
+    if len(new_contact) == len(contacts):
+        print("contact not found!\n")
+    else:
+        save_contact(new_contact)
+        print(f"Contact deleted\n")
+
+
+while True:
     print("Menu:")
     print("1. Store\n2. Display\n3. Delete\n4. Search\n5. Exit")
-
-    try:
-        menu_select = int(input("Enter Your Choice (1/ 2/ 3/ 4/ 5)? -> "))
-        if menu_select <1:
-            print("Invalid Option\n")
-    except ValueError:
-        print("Invalid Input\n")
-    
-    if menu_select == 1:
-        print(Fore.GREEN + "\n   Welcome to Storing Page")
-        print("-------------------------------")
-
-        #storing contact information(name, phone number & city)
-        while contact_info_saved == False:
-            contact_name = input(Fore.YELLOW + "Name -> " + Fore.RESET)
-            #phone number invalid
-            while True:
-                phone_number = input(Fore.YELLOW + "Phone Number -> " + Fore.RESET)
-                if len(phone_number) == 10 and phone_number.isdigit():
-                    break
-                else:
-                    print(Fore.RED + "Invalid Input!!")
-
-            city = input(Fore.YELLOW + "City -> " + Fore.RESET)
-            
-            edit_option = int(input(Fore.RED + "\nDo you want to save or change information? (1/ 2) -> " + Fore.RESET))
-            if edit_option == 1:
-                information["name"].append(contact_name)
-                information["phone_number"].append(phone_number)
-                information["city"].append(city)
-                contact_info_saved = True
-                print(Fore.LIGHTGREEN_EX + "Information Saved!!\n" + Fore.RESET)
-                with open("data.json", "w") as file:
-                    json.dump(information, file)
-                break
-            else:
-                print(Fore.BLUE + "\nChanging Information:" + Fore.RESET)
-    
-    if menu_select == 2:
-        if not os.path.exists("data.json"):
-            print(Fore.RED + "File doesn't exist")
+    choose = int(input("Choose -> "))
+    if choose == 1:
+        add_contact()
+    elif choose == 2:
+        display_contact()
+    elif choose == 3:
+        delete_contact()
+    elif choose == 4:
+        search_contact()
+    elif choose == 5:
+        print("Thank you")
+        break
+    else:
+        print("invalid input")
         
-        elif os.path.getsize("data.json") == 0:
-            print(Fore.RED + "\nNothing has been saved Yet\n")
-        
-        else:
-            with open("data.json", "r") as file:
-                print("\n")
-                data = json.load(file)
-                print(Fore.MAGENTA +"Your Overall Data:")
-                print(data)
-                print("\n")
